b78c2b33a569b1769f191a394ff0f60afca8b668
Ochkalov <ochkalov@gmail.com>
2017-09-23 13:23:13 +0300

    Find developers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { SearchUsersService } from "./search-users.service";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  place: string;
+  language: string;
+  results: any[] = [];
+  selected: boolean = false;
+  selectedUser: any;
+  error_text: string = "";
+
+  constructor(private searchService: SearchUsersService) { }
+
+  ngOnInit() {}
+
+  search(place: string, language: string) {
+    this.selected = false;
+    this.error_text = "";
+    if (place || language) {
+      this.place = place;
+      this.language = language;
+      this.searchService.getUsersByPlaceAndLanguage(place, language).subscribe(
+        users => {
+          this.results = users;
+        },
+        error => {
+          this.results = [];
+          this.error_text = "Sorry! No Users found. Try again";
+          console.error(error);
+        }
+      )
+    }
+  }
+
+  getDetails(username: string) {
+    this.searchService.getDetailsByUserName(username).subscribe(
+      userDatils => {
+        this.selectedUser = userDatils;
+        this.selected = true;
+      },
+      error => {
+        this.selected = false;
+        console.error(error);
+      }
+    )
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+
+import { AppComponent } from "./app.component";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { SearchUsersService } from "./search-users.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [ SearchUsersService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
